0d32ec1768208ddfe477be4096b09f42cf870151
eager-class-5814 <candacehchoe@gmail.com>
2020-06-06 23:12:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,5 +11,30 @@ class Song
     @genre = genre 
     @@count += 1 
     @@genres << genre 
+    @@artists << artist 
+  end 
+  
+  def self.count 
+    @@count 
+  end 
+  
+  def self.artists 
+    @@artists.uniq 
+  end 
+  
+  def self.genres
+    @@genres. uniq 
+  end 
+  
+  def self.genre_count 
+    genre_hash = {} 
+    @@genres.each do |genre| 
+      if genre_hash[genre] 
+        genre_hash += 1 
+      else 
+        genre_hash = 1 
+      end 
+    end 
+    genre_hash
   end 
 end 
